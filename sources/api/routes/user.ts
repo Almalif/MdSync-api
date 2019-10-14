7b64c96c846e323ec8560f7cd2b2b9bdbc8e9cda
alifdev <matthias.alif@epitech.eu>
2019-10-14 12:41:42 +0200

    Feature: Register route
    - This allows creating a user with a email and a password
    
    Resolve #9

--- sources/api/routes/user.ts
+++ sources/api/routes/user.ts
@@ -1,10 +1,43 @@
 import createRouter from "koa-joi-router";
 
+import UserService from "../../services/userService";
+import UserView from "../views/User";
+
+const { Joi } = createRouter;
+
 const router = createRouter();
 
 export default (app: createRouter.Router): createRouter.Router => {
   router.get("/", (ctx) => {
     ctx.body = "Users route";
+  });
+
+  router.route({
+    method: "post",
+    path: "/",
+    validate: {
+      body: {
+        email: Joi.string()
+          .lowercase()
+          .email(),
+        password: Joi.string()
+          .max(100)
+          .min(8),
+      },
+      type: "form",
+      output: {
+        201: {
+          body: {
+            email: Joi.string(),
+          },
+        },
+      },
+    },
+    handler: async (ctx) => {
+      const user = await UserService.register(ctx.request.body);
+      ctx.status = 201;
+      ctx.body = UserView.formatUser(user);
+    },
   });
 
   app.use("/users", router.middleware());
